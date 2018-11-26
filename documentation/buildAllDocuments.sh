5be2531f2fc340d47b1d82f412abb86da6114703
Philipp Rimmele (Oko) <philipp.rimmele@stud.htwk-leipzig.de>
2018-11-26 23:28:32 +0100

    Entwicklerdokumentation: - Information zum hinzufuegen von Dateien ins Git eingefuegt - Informationen zu LaTeX eingefuegt - Links aus dem Inhaltsverzeichniss eigefuegt. Sonstiges: - Script zum erstellen der Entwicklerdokumentation und des Benutzerhandbuchs eingefuegt

---
+++ documentation/buildAllDocuments.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/sh
+
+if [ ! -f projektdokumentation.aux ]
+then
+	echo "Bitte zuerst die projektdokumentation erzeugen"
+	exit 1
+fi
+
+pdflatex -jobname entwicklerdokumentation -synctex=1 -interaction=nonstopmode "\def\CurrentAudience{developer}\input{projektdokumentation}"
+pdflatex -jobname benutzerhandbuch -synctex=1 -interaction=nonstopmode "\def\CurrentAudience{manualDE}\input{projektdokumentation}"
+#Zweiter Build wegen Inhaltsverzeichniss
+pdflatex -jobname entwicklerdokumentation -synctex=1 -interaction=nonstopmode "\def\CurrentAudience{developer}\input{projektdokumentation}"
+pdflatex -jobname benutzerhandbuch -synctex=1 -interaction=nonstopmode "\def\CurrentAudience{manualDE}\input{projektdokumentation}"
+
