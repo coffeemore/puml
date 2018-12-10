2c7cfe58754b85b134d5eed0c2341af0ab04f217
Leo Rauschke <aelurus@outlook.de>
2018-12-11 00:28:31 +0100

    CodeCollector, Testfälle

--- code/src/CodeCollector.java
+++ code/src/CodeCollector.java
@@ -1,86 +1,178 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
 /**
  * 
- * @author 
- * Die Klasse dient zum Einsammeln des Quellcodes. 
- * Sie kann .java-, .jar-Dateien und komplette Ordner einsammeln.
- * Die Pfade zu den Dateien und den Ordnern werden von der Anwendung in das paths-Array geschrieben.
+ * @author Die Klasse dient zum Einsammeln des Quellcodes. Sie kann .java-,
+ *         .jar-Dateien und komplette Ordner einsammeln. Die Pfade zu den
+ *         Dateien und den Ordnern werden von der Anwendung in das paths-Array
+ *         geschrieben.
  */
 public class CodeCollector
 {
 
     /**
-     * Enthält die Pfade zu den Dateien und Ordnern
-     * Die Liste mit den Pfaden ist vor dem Aufrufen der getSourceCode-Methode zu füllen.
+     * Enthält die Pfade zu den Dateien und Ordnern Die Liste mit den Pfaden ist vor
+     * dem Aufrufen der getSourceCode-Methode zu füllen.
      */
     public ArrayList<String> paths;
-    
+
     /**
      * True = .java-Dateien werden verwendet; False = .java-Dateien werden ignoriert
      */
     private boolean useJavaFiles;
-    
+
     /**
      * True = .jar-Dateien werden verwendet; False = .jar-Dateien werden ignoriert
      */
     private boolean useJarFiles;
-
-
 
     /**
      * Konstruktor
      */
     public CodeCollector()
     {
-    };
+	paths = new ArrayList<String>();
+    }
 
-
-
-    
     /**
-     * Sammelt den Quellcode aus allen ausgewählten Dateien und gibt diesen als String zurück
+     * Sammelt den Quellcode aus allen ausgewählten Dateien und gibt diesen als
+     * String zurück
+     * 
      * @return String, der den vollständigen Quellcode enthält
      */
     public String getSourceCode()
     {
-	
-	
-	if (useJavaFiles && !useJarFiles) {
-	    
+	String sc = new String();
+	BufferedReader buffr = null;
+	FileReader filer = null;
+	if (!paths.isEmpty())
+	{
+	    if (useJavaFiles && !useJarFiles)
+	    {
+
+		for (int i = 0; i < paths.size(); i++)
+		{
+		    // Text in File in String einlesen
+		    // eingelesenes an sc ranhängen
+
+		    try
+		    {
+
+//        		buffr = new BufferedReader(new FileReader(FILENAME));
+			filer = new FileReader(paths.get(i));
+			buffr = new BufferedReader(filer);
+
+			String currLine;
+
+			while ((currLine = buffr.readLine()) != null)
+			{
+
+			    // ranhängen
+			    sc += currLine;
+			}
+
+		    } catch (IOException e)
+		    {
+			e.printStackTrace();
+		    }
+
+		    finally
+		    {
+			try
+			{
+			    if (buffr != null)
+				buffr.close();
+			    if (filer != null)
+				filer.close();
+			} catch (IOException ex)
+			{
+			    ex.printStackTrace();
+			}
+		    }
+		}
+	    } else
+	    {
+		if (useJarFiles && !useJavaFiles)
+		{
+		    try
+		    {
+
+//    		buffr = new BufferedReader(new FileReader(FILENAME));
+			filer = new FileReader(paths.get(0));
+			buffr = new BufferedReader(filer);
+
+			String currLine;
+
+			while ((currLine = buffr.readLine()) != null)
+			{
+			    // ranhängen
+			    sc += currLine;
+			}
+
+		    } catch (IOException e)
+		    {
+			e.printStackTrace();
+		    } finally
+		    {
+			try
+			{
+			    if (buffr != null)
+			    {
+				buffr.close();
+			    }
+			    if (filer != null)
+			    {
+				filer.close();
+			    }
+			} catch (IOException ex)
+			{
+			    ex.printStackTrace();
+			}
+		    }
+		}
+
+		else
+		{
+		    JOptionPane.showMessageDialog(null, "Bitte wählen Sie JAR- oder Java-Dateien aus.", "Error",
+			    JOptionPane.ERROR_MESSAGE);
+		    return null;
+		}
+	    }
+	    return sc;
+
 	}
-	if(useJarFiles && !useJavaFiles){
-	    
+
+	else
+	{
+
+	    JOptionPane.showMessageDialog(null, "Bitte wählen Sie einen Pfad aus.", "Error", JOptionPane.ERROR_MESSAGE);
+	    return null;
 	}
-	else {
-	    JOptionPane.showMessageDialog(null, "Bitte wählen Sie JAR- oder Java-Dateien aus.", "Error", JOptionPane.ERROR_MESSAGE); 
-	}
-	return new String("testcode");
-	
     }
-    
-    
+
     public boolean isUseJavaFiles()
     {
-        return useJavaFiles;
+	return useJavaFiles;
     }
 
     public void setUseJavaFiles(boolean useJavaFiles)
     {
-        this.useJavaFiles = useJavaFiles;
-    }
-    
-    public boolean isUseJarFiles()
-    {
-        return useJarFiles;
+	this.useJavaFiles = useJavaFiles;
     }
 
+    public boolean isUseJarFiles()
+    {
+	return useJarFiles;
+    }
 
     public void setUseJarFiles(boolean useJarFiles)
     {
-        this.useJarFiles = useJarFiles;
+	this.useJarFiles = useJarFiles;
     }
 
 }
