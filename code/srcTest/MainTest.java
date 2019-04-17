452481eafec87096b9381c33d99868c3e276c03e
marian <marian.geissler@stud.htwk-leipzig.de>
2019-04-17 14:25:43 +0200

    compareTextFile() Methode hinzugefuegt

--- code/srcTest/MainTest.java
+++ code/srcTest/MainTest.java
@@ -1,8 +1,11 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.apache.commons.cli.ParseException;
@@ -17,16 +20,15 @@ class MainTest {
 	{
 		classUnderTest = new PUMLgenerator();
 	}
-	@SuppressWarnings("static-access")
+	@SuppressWarnings({ "static-access", "unlikely-arg-type" })
 	@Test
 	void testMainCmd() throws IOException, ParseException //Test 1 einlesen Main aus Testdatensatz
 	{   
 		String[] testArgs = {"-c","-ijar","--i","./testfolder/datensatz/Main.java"};
 		classUnderTest.main(testArgs);
-		createOutputFile("@startuml\n" + 
-						"class Main\n" + 
-						"@enduml");
-		equals(true);
+		String[] reference = {"@startuml\n","class Main\n","@enduml"};
+		String filePath = "./outPUML_Code_defaultlocation";
+		equals(compareTextFile(reference,filePath));
 	 }
 	
 	void createOutputFile(String cont) throws IOException
@@ -37,4 +39,31 @@ class MainTest {
 		out.write(data.getBytes());
 		out.close();
     }
+	
+	@SuppressWarnings("resource")
+	boolean compareTextFile (String[] reference,String pumlFile) throws IOException
+	{
+		FileReader fr = new FileReader(pumlFile);
+		BufferedReader br = new BufferedReader(fr);
+		
+		String line = "";
+		int i = 0;
+		boolean endFile = false;
+		while(!endFile);
+		{
+			line = br.readLine();
+			if (line ==null)
+			{
+				endFile = true;
+			}
+			System.out.println(line);
+			if (!(line.equals(reference[i])) && !endFile)
+			{
+				return false;
+			}
+			i++;
+		}
+		br.close();
+		return true;
+	}
 }
