9b9e4e5599c71c279314e429e65d005c4d776b60
Elisabeth Schuster <elisabeth.schuster@stud.htwk-leipzig.de>
2019-05-03 18:28:02 +0200

    Einlesen von parsedData erweitert + Testfunktion zum Anzeigen der Knoten in Zusammenarbeit mit Leo

--- code/src/SequenceDiagramGenerator.java
+++ code/src/SequenceDiagramGenerator.java
@@ -1,4 +1,6 @@
 
+
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -17,7 +19,7 @@ public class SequenceDiagramGenerator {
      */
     public SequenceDiagramGenerator()
     {
-	
+
     }
     
     /**
@@ -45,7 +47,12 @@ public class SequenceDiagramGenerator {
 	Element entrypoint = seqDiagramm.createElement("entrypoint");
 	seq.appendChild(entrypoint);
 	
+	Element root1 = parsedData.getDocumentElement();
+	System.out.println(root1.getTagName());
+	
 	listMethoddef(parsedData, seqDiagramm, seq);
+	listAllNodes(root);
+	
 	
 	return null;
     }
@@ -61,11 +68,18 @@ public class SequenceDiagramGenerator {
 	//alle Einträge der Liste werden durchgegangen
 	for(int i = 0; i < cList.getLength(); i++) {
 	    Node cNode = cList.item(i);
+	    //System.out.println("Current Node: " + cNode.getNodeName());
 	    if(cNode.getNodeType() == Node.ELEMENT_NODE) {
 		//in Tag <name> gespeicherter Text wird als Klassenname übernommen
-		String cName = cNode.getFirstChild().getTextContent();
+		Element classEl = (Element) cNode;
+		//String cName = cNode.getFirstChild().getTextContent();
+		String cName = classEl.getElementsByTagName("name").item(0).getTextContent();
 		Element entry = seqDiagramm.createElement("entry");
 		entry.setTextContent(cName);
+		//System.out.println("TagName: "+entry.getTagName());
+		//-->entry
+		//System.out.println("TextContent: "+entry.getTextContent());
+		//-->Class1
 		classes.appendChild(entry);
 	    }
 	}
@@ -74,8 +88,29 @@ public class SequenceDiagramGenerator {
     private void listMethoddef(Document parsedData, Document seqDiagramm, Element seq) {
 	//Schleife, die Methoden auflistet
 	//kommt man so in die Unterpunkte <methoddefinition>?
+	
+	/*methoddefinition*/
+	Element method = seqDiagramm.createElement("methoddefinition");
+	seq.appendChild(method);
+	
+	//Liste mit den einzelnen Knoten der Methoden
 	NodeList mList = parsedData.getElementsByTagName("methoddefinition");
 	
+	for(int i = 0; i < mList.getLength(); i++) {
+	    //jeder einzelner Knoten wird herausgenommen
+	    Node mNode = mList.item(i);
+	    
+	    if(mNode.getNodeType() == Node.ELEMENT_NODE) {
+		
+		method.appendChild(seqDiagramm.importNode(mNode, true));
+		
+		/**String cName = e.getElementsByTagName("name").item(0).getTextContent();
+		Element entry = seqDiagramm.createElement("entry");
+		entry.setTextContent(cName);
+		classes.appendChild(entry);
+		*/
+	    }
+	}
 	/**
 	 * unter <methoddefinition> - name und alternatives kopieren
 	 * Rekursivität bei methodcalls 
@@ -83,5 +118,32 @@ public class SequenceDiagramGenerator {
 	
 	//Element methoddef = seqDiagramm.createElement("methoddefinition");
 	//seq.appendChild(methoddef);
+    }
+    
+    /**
+     * Bug: Fehler bei Textausgabe -> getTextContent listet allen Content der Unterknoten auf
+     * nicht nur der Childs
+     * @param root
+     */
+    private void listAllNodes(Element root) 
+    {
+	if(root.hasChildNodes()) 
+	{
+	    NodeList list = root.getChildNodes();
+	    for(int i=0; i<list.getLength();i++) 
+	    {
+		Node node = list.item(i);
+		
+		if(node.getNodeType()==Node.ELEMENT_NODE) 
+		{
+		    Element e = (Element) node;
+		    String m = e.getTagName();
+		    System.out.println(m);
+		    //System.out.println(e.getElementsByTagName(m).item(0).getTextContent());
+		    listAllNodes(e);
+		    System.out.println("/"+e.getTagName());
+		}
+	    }
+	}
     }
 }
