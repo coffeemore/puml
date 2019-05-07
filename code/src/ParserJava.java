d44bde3c487f0dbf5df30eca550c16a5ba1ed26c
Jona Meyer <jona.meyer@stud.htwk-leipzig.de>
2019-05-07 23:51:37 +0200

    Bugfix

--- code/src/ParserJava.java
+++ code/src/ParserJava.java
@@ -35,15 +35,17 @@ public class ParserJava implements ParserIf
     class TokenResult
     {
 
-	public TokenResult(int foundToken, String data)
+	public TokenResult(int foundToken, String data, String sourceCode)
 	{
 	    super();
 	    this.foundToken = foundToken;
 	    this.data = data;
+	    this.sourceCode = sourceCode;
 	}
 
 	private int foundToken;
 	private String data = "";
+	private String sourceCode = "";
 
 	public int getFoundToken()
 	{
@@ -65,6 +67,16 @@ public class ParserJava implements ParserIf
 	    this.data = data;
 	}
 
+	public String getSourceCode()
+	{
+	    return sourceCode;
+	}
+
+	public void setSourceCode(String sourceCode)
+	{
+	    this.sourceCode = sourceCode;
+	}
+
     }
 
     public TokenResult goToTokenWithName(String source, String[] name)
@@ -72,7 +84,7 @@ public class ParserJava implements ParserIf
 	String part = ""; // Variable wird genutztum zB Namen zu speichern
 	boolean found = false;
 	int foundNameIndex = 0;
-	// Erstes/Erste Zeichen werden auf die übertragenen Tokens überprüft
+	// Erstes/Erste Zeichen werden auf die ï¿½bertragenen Tokens ï¿½berprï¿½ft
 	for (int i = 0; i < name.length; i++)
 	{
 	    if (source.substring(0, name[i].length()).equals(name[i]))
@@ -95,21 +107,83 @@ public class ParserJava implements ParserIf
 		}
 	    }
 	}
-
-	return new TokenResult(foundNameIndex, part); // Rückgabe welcher Token gefunden wurde und den Inhalt zwischen
+	source=source.trim();
+	return new TokenResult(foundNameIndex, part, source); // Rï¿½ckgabe welcher Token gefunden wurde und den Inhalt zwischen
 						      // den Tokens (zB einen Klassennamen)
 
     }
 
-    public void findToken(String compString, String[] token2Array, int aktion)
+    public void findToken(String source)
     {
+	String[][] TokenArray = new String[10][10];
+	
+	TokenArray[0][0] = "\"";
+	TokenArray[0][1] = "\"";
+	TokenArray[0][2] = "\\\"";
 
+	TokenArray[1][0] = "//";
+	TokenArray[1][1] = "\n";
+
+	TokenArray[2][0] = "/*";
+	TokenArray[2][1] = "*/";
+
+	TokenArray[3][0] = "import ";
+	TokenArray[3][1] = ";";
+
+	TokenArray[4][0] = "class ";
+	TokenArray[4][1] = "extends";
+	TokenArray[4][2] = "implements";
+	TokenArray[4][3] = "{";
+
+	TokenArray[5][0] = "interface ";
+	TokenArray[5][1] = "{";
+	TokenArray[5][2] = "extends";
+
+	source = source.trim();
+
+	while (!source.isEmpty())
+	{
+
+	    boolean done = false;
+	    source = source.trim();
+	    for (int i = 0; i < TokenArray.length; i++)
+	    {
+
+		if (source.substring(0, TokenArray[i][0].length()).equals(TokenArray[i][0]))
+		{
+		   
+			source = source.substring(TokenArray[i][0].length());
+			source = source.trim();
+			TokenResult res;
+			do
+			{
+			    res = goToTokenWithName(source, TokenArray[i]);
+
+			    if (res.getFoundToken() != 1)
+			    {
+				source = source.substring(1);
+			    }
+			}
+			while (res.getFoundToken() == 1);
+			source = source.substring(1); // Entfernen des ersten Zeichens
+			done = true;
+		    
+		}
+	    }
+
+	    if (!done)
+	    {
+		source = source.substring(1);
+	    }
+	}
+	
+	
     }
 
     /**
-     * Entfernt Kommentare aus übergebenem String
+     * Entfernt Kommentare aus ï¿½bergebenem String
      * 
-     * @param sourcece übergebener String aus dem Kommentare entfernt werden
+     * @param sourcece ï¿½bergebener String aus dem Kommentare entfernt werden
      * @return XMl-Dokument
      * @throws ParserConfigurationException
      */
@@ -132,7 +206,7 @@ public class ParserJava implements ParserIf
 	{
 	    while (!sourcec.isEmpty())
 	    {
-		sourcec = sourcec.trim(); // Entfernen von Leerzeichen und Zeilenumbrüchen
+		sourcec = sourcec.trim(); // Entfernen von Leerzeichen und Zeilenumbrï¿½chen
 		boolean done = false;
 
 		compString = "\""; // Entfernen von Strings zur Vermeidung von Problemen beim Entfernen von
@@ -141,23 +215,26 @@ public class ParserJava implements ParserIf
 		{
 		    sourcec = sourcec.substring(compString.length());
 		    sourcec = sourcec.trim();
-		    String[] nameArray = new String[2];
-		    nameArray[0] = "\\\"";
-		    nameArray[1] = "\"";
+		    String[] nameArray = new String[3];
+		    nameArray[0] = "\\\\";
+		    nameArray[1] = "\\\"";
+		    nameArray[2] = "\"";
 
 		    TokenResult res;
 		    // Gehe durch den Sourcecode, bis kein String mehr gefunden wurde
 		    do
 		    {
 			res = goToTokenWithName(sourcec, nameArray);
+			sourcec= res.getSourceCode();
 			// System.out.println("@res: " + res.getData());
-			if (res.getFoundToken() == 0)
-			{
-			    sourcec = sourcec.substring(1);
+			if(res.getFoundToken()!= 2) {
+			    sourcec = sourcec.substring(2);
 			}
+			
+			
 		    }
-		    while (res.getFoundToken() != 1);
-		    sourcec = sourcec.substring(1); // Entfernen des ersten Zeichens
+		    while (res.getFoundToken() != 2);
+		    sourcec = sourcec.substring(1);
 		    done = true;
 		}
 		;
@@ -165,11 +242,13 @@ public class ParserJava implements ParserIf
 		compString = "//";
 		if (sourcec.substring(0, compString.length()).equals(compString))
 		{
+		    TokenResult res;
 		    sourcec = sourcec.substring(compString.length());
 		    sourcec = sourcec.trim();
 		    String[] nameArray = new String[1];
 		    nameArray[0] = "\n";
-		    goToTokenWithName(sourcec, nameArray);
+			res = goToTokenWithName(sourcec, nameArray);
+			sourcec= res.getSourceCode();
 		    done = true;
 		}
 		;
@@ -178,11 +257,13 @@ public class ParserJava implements ParserIf
 		compString = "/*";
 		if (sourcec.substring(0, compString.length()).equals(compString))
 		{
+		    TokenResult res;
 		    sourcec = sourcec.substring(compString.length());
 		    sourcec = sourcec.trim();
 		    String[] nameArray = new String[1];
 		    nameArray[0] = "*/";
-		    goToTokenWithName(sourcec, nameArray);
+			res = goToTokenWithName(sourcec, nameArray);
+			sourcec= res.getSourceCode();
 		    done = true;
 		}
 		;
@@ -190,11 +271,13 @@ public class ParserJava implements ParserIf
 		compString = "import ";
 		if (sourcec.substring(0, compString.length()).equals(compString))
 		{
+		    TokenResult res;
 		    sourcec = sourcec.substring(compString.length());
 		    sourcec = sourcec.trim();
 		    String[] nameArray = new String[1];
 		    nameArray[0] = ";";
-		    goToTokenWithName(sourcec, nameArray);
+			res = goToTokenWithName(sourcec, nameArray);
+			sourcec= res.getSourceCode();
 		    done = true;
 		}
 		;
@@ -314,13 +397,14 @@ public class ParserJava implements ParserIf
     }
 
     /**
-     * Liest den übergebenen Quellcode ein und parsed die Informationen daraus
+     * Liest den ï¿½bergebenen Quellcode ein und parsed die Informationen daraus
      * 
-     * @param sourceCode Vollständiger Java-Quellcode
+     * @param sourceCode Vollstï¿½ndiger Java-Quellcode
      */
     public void parse(String sourceCode)
     {
 	sourceCode.trim();
+	System.out.println(sourceCode);
 	try
 	{
 	    buildTree(sourceCode);
