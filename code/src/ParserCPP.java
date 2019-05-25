749588de89e6266398334bfc34243ebf48dc885f
jsollman <jan.sollmann@stud.htwk-leipzig.de>
2019-05-26 01:31:11 +0200

    ParserCPP nach Vorbild von ParserJava strukturiert

--- code/src/ParserCPP.java
+++ code/src/ParserCPP.java
@@ -1,17 +1,71 @@
 import org.w3c.dom.Document;
 
-public class ParserCPP implements ParserIf {
-
-	@Override
-	public void parse(String sourceCode) {
+public class ParserCPP implements ParserIf 
+{
+	private Document document;
+	
+	//Getter- und Settermethoden 
+	public Document getDocument() 
+	{
+		return document;
+	}
+	public void setDocument(Document document) 
+	{
+		this.document = document;
+	}
+	
+	//Hilfsklasse TokenResult: 
+	class TokenResult
+    {
+		private int foundToken;
+		private String data, sourceCode;
+		
+		public TokenResult(int foundToken, String data, String sourceCode)
+		{
+		    super();
+		    this.foundToken = foundToken;
+		    this.data = data;
+		    this.sourceCode = sourceCode;
+		}
+	
+		//Getter- und Settermethoden
+		public int getFoundToken() 
+		{
+			return foundToken;
+		}
+		public void setFoundToken(int foundToken) 
+		{
+			this.foundToken = foundToken;
+		}
+		public String getData() 
+		{
+			return data;
+		}
+		public void setData(String data) 
+		{
+			this.data = data;
+		}
+		public String getSourceCode() 
+		{
+			return sourceCode;
+		}
+		public void setSourceCode(String sourceCode) 
+		{
+			this.sourceCode = sourceCode;
+		}
+    }
+	
+	@Override //Parsed den übergeben Source-Code
+	public void parse(String sourceCode) 
+	{
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
-	public Document getParsingResult() {
-		// TODO Auto-generated method stub
-		return null;
+	@Override //Übergibt das Parsing-Ergebnis (document)
+	public Document getParsingResult() 
+	{
+		return document;
 	}
 
 }
