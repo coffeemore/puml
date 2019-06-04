bad95b06378938149dd6c807357319ce35589042
Michael Lux <Michael.lux@stud.htwk-leipzig.de>
2019-06-04 16:26:53 +0200

    Duplikate in Kompositions- und Agregations-Elementen der XML-Struktur entfernt

--- code/src/ParserJava.java
+++ code/src/ParserJava.java
@@ -1,4 +1,5 @@
 import java.io.StringWriter;
+
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -12,12 +13,13 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 /**
  * 
  * @author Klasse, die den Parser f�r Java implementiert
  */
-public class ParserJava implements ParserIf
+public class ParserJava extends XmlHelperMethods implements ParserIf
 {
     Document document;
 
@@ -380,6 +382,10 @@ public class ParserJava implements ParserIf
 			// compString.length()).equals(compString)));
 
 		    }
+		    Element compNode = document.createElement("composition");
+		    curNode.appendChild(compNode);
+		    Element agrNode = document.createElement("agregation");
+		    curNode.appendChild(agrNode);
 		    done = true;
 		}
 		compString = "interface ";
@@ -433,13 +439,19 @@ public class ParserJava implements ParserIf
 			extendsName = extendsName.strip();
 			System.out.println("@extendsName: " + extendsName);
 		    }
+		    Element compNode = document.createElement("composition");
+		    curNode.appendChild(compNode);
+		    Element agrNode = document.createElement("agregation");
+		    curNode.appendChild(agrNode);
 		    done = true;
 		}
 		;
 		////////////
 		if (curNode.getNodeName().equals("classdefinition")
-			|| curNode.getNodeName().equals("interfacedefinition")||curNode.getNodeName().equals("methoddefinition")
-			|| curNode.getNodeName().equals("constructor"))// &&
+			|| curNode.getNodeName().equals("interfacedefinition")
+		// || curNode.getNodeName().equals("methoddefinition")
+		// || curNode.getNodeName().equals("constructor"))// &&
+		)
 		// (sourcec.charAt(0)==';'||sourcec.charAt(0)=='{')
 		{// TODO: funktionen in funktionen haben nicht klassen als eltern
 
@@ -448,8 +460,8 @@ public class ParserJava implements ParserIf
 
 		    TokenResult res1 = goToTokenWithName(sourcec, nameArray);
 		    String functionData = res1.getData().strip();
-		    functionData=functionData.replaceAll("\n", " ");
-		    functionData=functionData.replaceAll(" +", " ");
+		    functionData = functionData.replaceAll("\n", " ");
+		    functionData = functionData.replaceAll(" +", " ");
 		    String[] prefixRBrace = functionData.split(" ");
 
 		    switch (prefixRBrace.length)
@@ -519,8 +531,11 @@ public class ParserJava implements ParserIf
 				nameArray2[0] = ")";
 				nameArray2[1] = ",";
 				TokenResult res2;
-				Element classComposition = document.createElement("composition");
-				curNode.appendChild(classComposition);
+
+				// Composition-knoten erstellen
+
+				Element classObject = (Element) getChildwithName(curNode, "agregation");
+
 				do
 				{
 
@@ -530,9 +545,21 @@ public class ParserJava implements ParserIf
 				    String[] argumentConstructor = functionData2.split(" ");
 				    if (argumentConstructor.length == 2)
 				    {
-					Element classCompositionEl = document.createElement("entry");
-					classComposition.appendChild(classCompositionEl);
-					classCompositionEl.appendChild(document.createTextNode(argumentConstructor[0]));
+					boolean inAgregation = false;
+					for (int i = 0; i < curNode.getElementsByTagName("entry").getLength(); i++)
+					{
+					    if (curNode.getElementsByTagName("entry").item(i).getTextContent()
+						    .equals(argumentConstructor[0]))
+						inAgregation = true;
+					    ;
+					}
+					if (inAgregation == false)
+					{
+					    Element classCompositionEl = document.createElement("entry");
+					    classObject.appendChild(classCompositionEl);
+					    classCompositionEl
+						    .appendChild(document.createTextNode(argumentConstructor[0]));
+					}
 				    }
 				    sourcec = res2.getSourceCode();
 				    sourcec = sourcec.substring(1);
@@ -546,8 +573,8 @@ public class ParserJava implements ParserIf
 				{
 				    sourcec = sourcec.substring(1);
 				    curlBrace++;
-				    Element constNode = document.createElement("constructor");
-				    curNode.appendChild(constNode);
+				    // Element constNode = document.createElement("constructor");
+				    // curNode.appendChild(constNode);
 				    curNode = (Element) curNode.getLastChild();
 
 				}
@@ -688,10 +715,10 @@ public class ParserJava implements ParserIf
 //			    curNode = (Element) curNode.getLastChild();
 			    break;
 			}
-			
+
 			if ((prefixRBrace[1].equals("=") || prefixRBrace[2].equals("new")))
 			{
-			    //Instance-knoten erstellen
+			    // Instance-knoten erstellen
 			    Element instanceNode = document.createElement("instance");
 			    curNode.appendChild(instanceNode);
 			    Element instanceNNode = document.createElement("name");
@@ -700,19 +727,26 @@ public class ParserJava implements ParserIf
 			    Element instanceCNode = document.createElement("class");
 			    instanceNode.appendChild(instanceCNode);
 			    instanceCNode.appendChild(document.createTextNode(prefixRBrace[3]));
-			    
-			    
-			    //Composition-knoten erstellen
-			    Element classObject = document.createElement("composition");
-			    curNode.appendChild(classObject);
 
-			    Element classObjectEl = document.createElement("entry");
-			    classObjectEl.appendChild(document.createTextNode(prefixRBrace[3]));
-			    classObject.appendChild(classObjectEl);
-			    // curNode = (Element) curNode.getLastChild();
+			    Element classObject = (Element) getChildwithName(curNode, "composition");
+
+			    boolean inAgregation = false;
+			    for (int i = 0; i < curNode.getElementsByTagName("entry").getLength(); i++)
+			    {
+				if (curNode.getElementsByTagName("entry").item(i).getTextContent()
+					.equals(prefixRBrace[3]))
+				    inAgregation = true;
+				;
+			    }
+			   
+			    if (inAgregation == false)
+			    {
+				Element classObjectEl = document.createElement("entry");
+				classObjectEl.appendChild(document.createTextNode(prefixRBrace[3]));
+				classObject.appendChild(classObjectEl);
+				// curNode = (Element) curNode.getLastChild();
+			    }
 			    sourcec = res1.getSourceCode();
-			    
-			    
 			}
 			break;
 
@@ -721,7 +755,6 @@ public class ParserJava implements ParserIf
 			break;
 		    }
 		}
-
 
 //		    else
 //		    {
@@ -922,8 +955,8 @@ public class ParserJava implements ParserIf
      */
     public void parse(ArrayList<String> sourceCode)
     {
-	sourceCode.trim();
-	//sourceCode = sourceCode.replaceAll("=", " = ");
+	sourceCode.get(0).trim();
+	// sourceCode = sourceCode.replaceAll("=", " = ");
 	System.out.println(sourceCode);
 	try
 	{
