7030bcfbf7e2791f622fc6f8ab0acc45e2357c17
tarndt <tore.arndt@stud.htwk-leipzig.de>
2019-06-23 21:56:48 +0200

    Testfälle für KlassenD + SeqD erstellt [überprüfen ob korrekt muss noch überarbeitet werden]

--- code/srcTest/GesamtTest.java
+++ code/srcTest/GesamtTest.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.commons.cli.ParseException;
 import org.junit.internal.runners.TestClass;
@@ -17,11 +20,11 @@ class GesamtTest
     @AfterAll
     static void tearDownAfterClass() throws Exception
     {
-	File here = new File(".");
+	File here = new File("testfolder/tempData/.");
 	System.out.println("Waehrend des Tests erzeugte Testfiles unter "+ here.getAbsolutePath()+ " sollen gelöscht werden.");
-	File defaultFile = new File("outPUML_Code_defaultlocation");
-	File givenOutFile = new File("testfolder/datensatz/TestMainoutPUML_Code");
-	if (defaultFile.delete() || givenOutFile.delete())
+	File cDFile = new File("testfolder/tempData/CDoutPUML_Graph");
+	File sQFile = new File("testfolder/tempData/SQoutPUML_Graph");
+	if (sQFile.delete() && cDFile.delete())
 	{
 		System.out.println("Testfile erfolgreich nach Test gelöscht.");
 	}
@@ -33,16 +36,22 @@ class GesamtTest
 
     @SuppressWarnings({ "static-access" })
     @Test
-    void test() throws IOException, ParseException
+    void testSQ() throws IOException, ParseException
     {
 	testClass = new PUMLgenerator();
+	//ARGS : -c -ijar -ct -cs Class1,method1  - -i testfolder/xmlSpecifications/sources/java/Class1.java -o testfolder/xmlSpecifications
 	String[] specification = {
 		"-c",
 		"-ijar",
+		"-ct" ,
+		"-cs" ,
+		"Class1,method1",
 		"--i",
-		"testfolder/xmlSpecifications/sources/java/Class1.java",
+		"testfolder/xmlSpecifications/sources/java/Class1.java" ,
+		"testfolder/xmlSpecifications/sources/java/Class2.java;" ,
+		"testfolder/xmlSpecifications/sources/java/Class3.java" ,
 		"-o", //erstellt Ordener, wenn nicht vorhanden??
-		"testfolder/tests"
+		"testfolder/tempData/SQ"
 		
 	};
 	testClass.main(specification);
@@ -63,5 +72,69 @@ class GesamtTest
 	assertEquals(expected, actual);
 	//fail("Not yet implemented");
     }
+    
+    @SuppressWarnings({ "static-access" })
+    @Test
+    void testCD() throws IOException, ParseException
+    {
+	testClass = new PUMLgenerator();
+	//ARGS : -c -ijar -ct -cs Class1,method1  - -i testfolder/xmlSpecifications/sources/java/Class1.java -o testfolder/xmlSpecifications
+	String[] specification = {
+		"-c",
+		"-ijar",
+		"-ct" ,
+		"-cc" ,
+		//"Class1,method1",
+		"--i",
+		"testfolder/xmlSpecifications/sources/java/Class1.java" ,
+		//"testfolder/xmlSpecifications/sources/java/Class2.java;" ,
+		//"testfolder/xmlSpecifications/sources/java/Class3.java" ,
+		"-o", //erstellt Ordener, wenn nicht vorhanden??
+		"testfolder/tempData/CD"
+		
+	};
+	testClass.main(specification);
+	/*
+	 * Expected File wird gelesen und mit einem StringBuilder in einen String ("expected") geschrieben
+	 */
+	FileReader fr = new FileReader(new File("testfolder/xmlSpecifications/SeqDiagramNew.txt"));
+	BufferedReader br = new BufferedReader(fr);
+	StringBuilder sb = new StringBuilder();
+	
+	for (String line = br.readLine(); line != null; line = br.readLine())
+	{
+	    sb.append(line).append(line).append("\n");
+	}
+	String expected = sb.toString();
+	
+	String actualPath = "testfolder/tempData/SQoutPUML_Code";//TODO Actual File Beschreiben
+	assertTrue(compareTextFile(expected, actualPath));
+	//fail("Not yet implemented");
+    }
+    
+    @SuppressWarnings("resource")
+	boolean compareTextFile (String expected,String pumlFile) throws IOException
+	{
+			FileReader fr = new FileReader(pumlFile);
+			BufferedReader br = new BufferedReader(fr);
+			
+			StringBuilder sb = new StringBuilder();
+			
+			for (String line = br.readLine(); line != null; line = br.readLine())
+			{
+			    sb.append(line).append(line).append("\n");
+			}
+			String actual = sb.toString();
+			System.out.println("/////////////////////\n\n" + actual + "\n\n/////////////////////");
+			
+			if (expected.equals(actual))
+			{
+			    return true;
+			}
+			else
+			{
+			    return false;
+			}
+	}
 
 }
