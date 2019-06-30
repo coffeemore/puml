b9002f1fea68b33586e16c4da69968cf135a9507
Johann Gerhardt <johann.gerhardt@stud.htwk-leipzig.de>
2019-06-30 12:39:47 +0200

    .

--- code/src/ParserCPP.java
+++ code/src/ParserCPP.java
@@ -3,8 +3,6 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class ParserCPP implements ParserIf
 {
