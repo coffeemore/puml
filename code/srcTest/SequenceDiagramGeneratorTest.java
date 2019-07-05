fa7453f7f34185c2f00eed01a7e6a00a2e8bb384
srauschk <samanta_leonie.rauschke@stud.htwk-leipzig.de>
2019-07-05 12:13:53 +0200

    Update SequenceDiagramGeneratorTest.java

--- code/srcTest/SequenceDiagramGeneratorTest.java
+++ code/srcTest/SequenceDiagramGeneratorTest.java
@@ -28,45 +28,45 @@ class SequenceDiagramGeneratorTest
 	classUnderTest = new SequenceDiagramGenerator();
 	parsedData = xmlHM.getDocumentFrom("..//code//testfolder//xmlSpecifications//parsedData.xml");
     }
-//
-//    @Test
-//    void test1() throws TransformerException, XPathExpressionException, ParserConfigurationException, SAXException,
-//	    IOException
-//    {
-//	// Test, ob das erstellte Document mit der Spezifikation übereinstimmt
-//	assertAll(() ->
-//	{
-//	    Document test = classUnderTest.createDiagram(parsedData, "Class1", "method1");
-//	    test = classUnderTest.createDiagram(parsedData, "Class1", "method1");
-//	    
-//	    Document seqDiagram = xmlHM.getDocumentFrom("..//code//testfolder//xmlSpecifications//SeqDiagram.xml");
-//	    boolean s = false;
-//	    s = xmlHM.compareXML(seqDiagram, test);
-//	    assertTrue(s);
-//	}, () ->
-//	// Test, ob das erstellte Document NICHT mit der Spezifikation übereinstimmt
-//	{
-//	    Document test = classUnderTest.createDiagram(parsedData, "Class2", "method1");
-//	    Document seqDiagram = xmlHM.getDocumentFrom("..//code//testfolder//xmlSpecifications//SeqDiagram.xml");
-//	    boolean s = false;
-//	    s = xmlHM.compareXML(seqDiagram, test);
-//	    assertFalse(s);
-//	});
-//    }
-//
-//    // Test mit dem vom Parser erstellen Dokument
-//    // Voraussetzung, dass die Datei mit den richtigen Daten vorhanden ist
-//    @Test
-//    void test2() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException,
-//	    TransformerException
-//    {
-//	Document source = xmlHM.getDocumentFrom("//home//developer//tempLogger//PUMLlog.xml");
-//	
-//	Document test = classUnderTest.createDiagram(source, "Class1", "method1");
-//	Document seqDiagram = xmlHM.getDocumentFrom("..//code//testfolder//xmlSpecifications//SeqDiagram.xml");
-//	boolean m = xmlHM.compareXML(seqDiagram, test);
-//	assertTrue(m);
-//    }
+
+    @Test
+    void test1() throws TransformerException, XPathExpressionException, ParserConfigurationException, SAXException,
+	    IOException
+    {
+	// Test, ob das erstellte Document mit der Spezifikation übereinstimmt
+	assertAll(() ->
+	{
+	    Document test = classUnderTest.createDiagram(parsedData, "Class1", "method1");
+	    test = classUnderTest.createDiagram(parsedData, "Class1", "method1");
+	    
+	    Document seqDiagram = xmlHM.getDocumentFrom("..//code//testfolder//xmlSpecifications//SeqDiagram.xml");
+	    boolean s = false;
+	    s = xmlHM.compareXML(seqDiagram, test);
+	    assertTrue(s);
+	}, () ->
+	// Test, ob das erstellte Document NICHT mit der Spezifikation übereinstimmt
+	{
+	    Document test = classUnderTest.createDiagram(parsedData, "Class2", "method1");
+	    Document seqDiagram = xmlHM.getDocumentFrom("..//code//testfolder//xmlSpecifications//SeqDiagram.xml");
+	    boolean s = false;
+	    s = xmlHM.compareXML(seqDiagram, test);
+	    assertFalse(s);
+	});
+    }
+
+    // Test mit dem vom Parser erstellen Dokument
+    // Voraussetzung, dass die Datei mit den richtigen Daten vorhanden ist
+    @Test
+    void test2() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException,
+	    TransformerException
+    {
+	Document source = xmlHM.getDocumentFrom("//home//developer//tempLogger//PUMLlog.xml");
+	
+	Document test = classUnderTest.createDiagram(source, "Class1", "method1");
+	Document seqDiagram = xmlHM.getDocumentFrom("..//code//testfolder//xmlSpecifications//SeqDiagram.xml");
+	boolean m = xmlHM.compareXML(seqDiagram, test);
+	assertTrue(m);
+    }
 
     @Test
     void testEPClassFirst() throws XPathExpressionException
